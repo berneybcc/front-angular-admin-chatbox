e0adc2f33585749f1adef9a75c263ada9c2e0c09
berneybcc <57729351+berneybcc@users.noreply.github.com>
2021-10-21 15:32:48 -0500

    Reporte en excel

--- src/app/graficas-report/graficas-report.component.ts
+++ src/app/graficas-report/graficas-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SaveDatosService } from '../services/save-datos.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-graficas-report',
@@ -8,9 +9,11 @@ import { SaveDatosService } from '../services/save-datos.service';
 })
 export class GraficasReportComponent implements OnInit {
 
+  fileName="Informe.xlsx";
   title = 'Consultas realizadas';
   type = 'ColumnChart';
   data= [];
+  info:any=[];
   options = {   
     hAxis: {
         title: 'Descripcion'
@@ -35,11 +38,39 @@ export class GraficasReportComponent implements OnInit {
   infoCharts(){
       this.api.obtainCharts().subscribe((data:any)=>{
         if(!data.error){
+          
           for(var d of (data.data as any)){    
             var arrayInfo = [d.description_question,d.total];
+            this.info.push({descripction:d.description_question,consultas:d.total});
             this.data.push(arrayInfo);
           }    
         }
       })
   }
+
+  exportexcel(): void 
+    {
+      import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.info); // Sale Data
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "report");
+    });		
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    import("file-saver").then(FileSaver => {
+      let EXCEL_TYPE =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+      let EXCEL_EXTENSION = ".xlsx";
+      const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE
+      });
+      FileSaver.saveAs(
+        data,
+        fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+      );
+    });
+  }
+
 }
